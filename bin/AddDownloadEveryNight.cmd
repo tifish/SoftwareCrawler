2777aacebdc6a7b00e459ff5e1055ba2951bf8a5
Tinyfish <tifish@gmail.com>
2022-05-24 01:18:01 +0800

    Start download at 4:00 to avoid maintaining time.

--- bin/AddDownloadEveryNight.cmd
+++ bin/AddDownloadEveryNight.cmd
@@ -1,3 +1,3 @@
 @echo off
-schtasks /create /f /tn "My\Software Crawler" /sc daily /st 01:00 /tr "'%~dp0SoftwareCrawler.exe' --download-all --auto-close"
+schtasks /create /f /tn "My\Software Crawler" /sc daily /st 04:00 /tr "'%~dp0SoftwareCrawler.exe' --download-all --auto-close"
 pause
